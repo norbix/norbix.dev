0619ef08327a26eb863659ee10e6f545ac018e12
norbix <norbix@users.noreply.github.com>
2025-07-24 17:54:00 +0000

    deploy: 257540f8e4d1302c2e8b95b1cb4153f26b393181

---
+++ posts/algorithms-and-data-structures/tests/merge_sort_test.go
@@ -0,0 +1,45 @@
+package tests
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func mergeSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	mid := len(arr) / 2
+	left := mergeSort(arr[:mid])
+	right := mergeSort(arr[mid:])
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	result := []int{}
+	i, j := 0, 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] < right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+	}
+	return append(result, append(left[i:], right[j:]...)...)
+}
+
+func Test_mergeSort(t *testing.T) {
+	fixture := []int{4, 2, 7, 1, 0}
+	expected := []int{0, 1, 2, 4, 7}
+
+	result := mergeSort(fixture)
+
+	assert.Equal(t, expected, result)
+	fmt.Println(result)
+}
